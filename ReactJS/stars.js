8262fcab813a0460b1b1e504974509fcfcbc5d74
Rafael <rafaelsua@gmail.com>
2017-09-25 12:22:58 +0200

    Update stars.js
    Numero de respuesta aleatorios generados una sola vez con la libreria https://lodash.com/

--- ReactJS/stars.js
+++ ReactJS/stars.js
@@ -1,13 +1,11 @@
 const Stars = (props) => {
 	const numberOfStars = 1 + Math.floor(Math.random()*9);
-  
-  let stars = [];
-  for(let i=0; i< numberOfStars; i++){
-  	stars.push(<i className="fa fa-star"></i>);
-  }
+ 
 	return(
   	<div className="col-5">
-    	{stars}
+    	{_.range(numberOfStars).map(i =>
+      	<i key={i} className="fa fa-star"></i>
+      )}
     </div>
   );	
 }
@@ -23,22 +21,25 @@ const Button = (props) => {
 const Answer = (props) => {
 	return(
   	<div className="col-5">
-    	...
+    	<span>5</span>
+      <span>6</span>
     </div>
   );
 }
 
 const Numbers = (porps) => {
+	
 	return(
   	<div className="card text-center">
     	<div>
-      	<span>1</span>
-        <span className="selected">2</span>
-        <span className="used">3</span>
+      	{Numbers.list.map((number, i) =>
+        	<span key={i}>{number}</span>
+        )}
       </div>
     </div>
   );
 }
+Numbers.list = _.range(1, 10);
 
 class Game extends React.Component{
 	render(){
